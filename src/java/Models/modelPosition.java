99a324782baa6bfbac13785166346e113e3a09a4
arvention <amc.antioquia@yahoo.com.ph>
2015-03-18 08:07:48 +0800

    [MODEL]-Added Position Model-03182015_0807

---
+++ src/java/Models/modelPosition.java
@@ -0,0 +1,28 @@
+
+package models;
+
+public class modelPosition {
+    private int positionID;
+    private String name;
+
+    public modelPosition(int positionID, String name){
+        this.positionID = positionID;
+        this.name = name;
+    }
+    
+    public int getPositionID() {
+        return positionID;
+    }
+
+    public void setPositionID(int positionID) {
+        this.positionID = positionID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

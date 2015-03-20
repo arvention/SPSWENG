296401c670b51bda7314539a3341788579cb0eae
arvention <amc.antioquia@yahoo.com.ph>
2015-03-20 21:06:21 +0800

    [JAVA]-Branch Model-03202015_2106

---
+++ [SPSWENG] Equilibrium/src/java/Models/modelBranch.java
@@ -0,0 +1,39 @@
+package Models;
+
+
+public class modelBranch {
+    private int branchID;
+    private String name;
+    private String address;
+
+    public modelBranch(int branchID, String name, String address){
+        this.branchID = branchID;
+        this.name = name;
+        this.address = address;
+    }
+    
+    public int getBranchID() {
+        return branchID;
+    }
+
+    public void setBranchID(int branchID) {
+        this.branchID = branchID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+}

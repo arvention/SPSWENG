3b56b3619f88df0a4aa4c0ebed3725304de46d9e
arvention <amc.antioquia@yahoo.com.ph>
2015-03-21 00:06:52 +0800

    [JAVA]-Employee Audit Trail Model-03212015_0006

---
+++ [SPSWENG] Equilibrium/src/java/Models/modelEmployeeAuditTrail.java
@@ -0,0 +1,88 @@
+
+package Models;
+
+import java.sql.Timestamp;
+
+public class modelEmployeeAuditTrail {
+    private int empAuditTrailID;
+    private String fieldChanged;
+    private String editFrom;
+    private String editTo;
+    private int editorEntryNum;
+    private int editedEntryNum;
+    private Timestamp timestamp;
+    private boolean isApproved;
+    private int approverEntryNum;
+
+    public int getEmpAuditTrailID() {
+        return empAuditTrailID;
+    }
+
+    public void setEmpAuditTrailID(int empAuditTrailID) {
+        this.empAuditTrailID = empAuditTrailID;
+    }
+
+    public String getFieldChanged() {
+        return fieldChanged;
+    }
+
+    public void setFieldChanged(String fieldChanged) {
+        this.fieldChanged = fieldChanged;
+    }
+
+    public String getEditFrom() {
+        return editFrom;
+    }
+
+    public void setEditFrom(String editFrom) {
+        this.editFrom = editFrom;
+    }
+
+    public String getEditTo() {
+        return editTo;
+    }
+
+    public void setEditTo(String editTo) {
+        this.editTo = editTo;
+    }
+
+    public int getEditorEntryNum() {
+        return editorEntryNum;
+    }
+
+    public void setEditorEntryNum(int editorEntryNum) {
+        this.editorEntryNum = editorEntryNum;
+    }
+
+    public int getEditedEntryNum() {
+        return editedEntryNum;
+    }
+
+    public void setEditedEntryNum(int editedEntryNum) {
+        this.editedEntryNum = editedEntryNum;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isIsApproved() {
+        return isApproved;
+    }
+
+    public void setIsApproved(boolean isApproved) {
+        this.isApproved = isApproved;
+    }
+
+    public int getApproverEntryNum() {
+        return approverEntryNum;
+    }
+
+    public void setApproverEntryNum(int approverEntryNum) {
+        this.approverEntryNum = approverEntryNum;
+    }
+}

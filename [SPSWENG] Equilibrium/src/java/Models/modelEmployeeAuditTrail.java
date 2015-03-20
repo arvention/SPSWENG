dd6cdc5df5e7ab528c0791ede430a33ce9470a76
arvention <amc.antioquia@yahoo.com.ph>
2015-03-21 00:29:35 +0800

    [JAVA]-Employee Audit Trail Model-03212015_0029
    added constructor

--- [SPSWENG] Equilibrium/src/java/Models/modelEmployeeAuditTrail.java
+++ [SPSWENG] Equilibrium/src/java/Models/modelEmployeeAuditTrail.java
@@ -14,6 +14,19 @@ public class modelEmployeeAuditTrail {
     private boolean isApproved;
     private int approverEntryNum;
 
+    public modelEmployeeAuditTrail(int empAuditTrailID, String fieldChanged, String editFrom, String editTo,
+                                    int editorEntryNum, int editedEntryNum, Timestamp timestamp){
+        this.empAuditTrailID = empAuditTrailID;
+        this.fieldChanged = fieldChanged;
+        this.editFrom = editFrom;
+        this.editTo = editTo;
+        this.editorEntryNum = editorEntryNum;
+        this.editedEntryNum = editedEntryNum;
+        this.timestamp = timestamp;
+        isApproved = false;
+        approverEntryNum = 0;
+    }
+    
     public int getEmpAuditTrailID() {
         return empAuditTrailID;
     }

7d2608adbc5797a29821f1a2fb08f9004904b5a4
ebackers <arcestalavera@gmail.com>
2015-03-22 17:54:24 +0800

    US9-Complete-03222015_0554

---
+++ [SPSWENG] Equilibrium/src/java/Servlets/LeaveSubmit.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Equilibrium_Servlet;
+
+import Equilibrium_Classes.Database;
+import Equilibrium_Classes.EmailNotifier;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Arces
+ */
+public class LeaveSubmit extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LeaveSubmit</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LeaveSubmit at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        Database db = Database.getInstance();
+        EmailNotifier en = EmailNotifier.getInstance();
+
+        Date startDate = null;
+        Date endDate = null;
+        try {
+            startDate = new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("startDate"));
+            endDate = new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("endDate"));
+
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+
+        HttpSession reqSession = request.getSession();
+        RequestDispatcher reqDispatcher = null;
+        ArrayList<Boolean> errorFlags = new ArrayList<>();
+        int empIDNum = Integer.parseInt(request.getParameter("idNum"));
+        
+        System.out.println("start: " + startDate + " end: " + endDate + " | "+ getDateDiff(startDate, endDate) + " vs " + (int) Float.parseFloat(request.getParameter("dayCount")));
+        
+        if (endDate != null && startDate != null) {
+            if (endDate.before(startDate)) {
+                errorFlags.add(true);
+                errorFlags.add(false);
+                errorFlags.add(false);                
+                
+                reqSession.setAttribute("errorFlags", errorFlags);
+                reqDispatcher = request.getRequestDispatcher("LeaveForm.jsp");
+            } else if (getDateDiff(startDate, endDate) > (int) Float.parseFloat(request.getParameter("dayCount"))) {
+                errorFlags.add(false);
+                errorFlags.add(true);
+                errorFlags.add(false);
+                
+                reqSession.setAttribute("errorFlags", errorFlags);
+                reqDispatcher = request.getRequestDispatcher("LeaveForm.jsp");
+            } else if(!db.checkEmpID(empIDNum)){
+                errorFlags.add(false);
+                errorFlags.add(false);
+                errorFlags.add(true);
+                
+                reqSession.setAttribute("errorFlags", errorFlags);
+                reqDispatcher = request.getRequestDispatcher("LeaveForm.jsp");
+            }else {
+                String leaveType = request.getParameter("leaveType");
+                float duration = Float.parseFloat(request.getParameter("dayCount"));
+                
+                db.addLeaveForm(leaveType, db.getEntryNum(empIDNum), new java.sql.Date(startDate.getTime()), duration);
+                reqDispatcher = request.getRequestDispatcher("LeaveSuccess.html");
+                
+                //send an email to the manager
+                en.sendLeaveRequest(db.getEntryNum(empIDNum), leaveType, startDate, endDate);
+            }
+            reqDispatcher.forward(request, response);
+        }
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    
+    // -- function/s ----------------------------------------
+    public int getDateDiff(Date startDate, Date endDate) {
+        int dateDiff = (int)(endDate.getTime() - startDate.getTime())/(1000 * 60 * 60 * 24);
+        
+        return dateDiff;
+    }
+}
--- [SPSWENG] Equilibrium/web/js/LeaveForm.js
+++ [SPSWENG] Equilibrium/web/js/LeaveForm.js
@@ -3,4 +3,43 @@ $(document).ready(function () {
         changeMonth: true,
         changeYear: true
     });
+
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    //initialize dates to today
+    if (month < 10)
+    {
+        $("input[name='startDate']").val(year + "-0" + month + "-" + day);
+        $("input[name='endDate']").val(year + "-0" + month + "-" + day);
+    }
+    else
+    {
+        $("input[name='startDate']").val(year + "-" + month + "-" + day);
+        $("input[name='endDate']").val(year + "-0" + month + "-" + day);
+    }
+
+    $("input[name='dayCount']").change(function() {
+        var tempDate = new Date($("input[name='startDate']").val());
+        var dayCount = Math.floor($("input[name='dayCount']").val());
+
+        tempDate.setDate(tempDate.getDate() + dayCount);
+
+        var tempMonth;
+        var tempDay;
+
+        if (tempDate.getMonth() + 1 < 10)
+            tempMonth = "-0" + (tempDate.getMonth() + 1);
+        else
+            tempMonth = "-" + (tempDate.getMonth() + 1);
+
+        if (tempDate.getDate() < 10)
+            tempDay = "-0" + tempDate.getDate();
+        else
+            tempDay = "-" + tempDate.getDate();
+
+        $("input[name='endDate']").val(tempDate.getFullYear() + tempMonth + tempDay);
+    });
 });

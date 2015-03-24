4afdc26e610f07fa14aed4ae0e9e5d6d0c67409d
ebackers <arcestalavera@gmail.com>
2015-03-25 02:10:44 +0800

    US2-Fixed Packages-03252015_0210

--- [SPSWENG] Equilibrium/src/java/Servlets/EnterBioData.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/EnterBioData.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Equilibrium_Servlet;
+package Servlets;
 
-import Equilibrium_Classes.Database;
+import Database.Database;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.ParseException;

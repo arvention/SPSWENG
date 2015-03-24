983d1cf828780e7957d5b30989af73d11bc8fce9
arvention <amc.antioquia@yahoo.com.ph>
2015-03-24 21:48:01 +0800

    US11-Database Password Error-03242015_2147
    Fixed bug in logging in using all capital letters.

--- [SPSWENG] Equilibrium/src/java/Database/Database.java
+++ [SPSWENG] Equilibrium/src/java/Database/Database.java
@@ -15,7 +15,7 @@ public class Database {
     private Statement stmt;
     private ResultSet rs;
     private static Database databaseInstance = new Database();
-    
+
     private Database() {
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -35,21 +35,22 @@ public class Database {
         return databaseInstance;
     }
 
-    public Connection getConnection(){
+    public Connection getConnection() {
         return this.con;
     }
-    
+
 // - FUNCTIONS -------------------------------------------------------------------------------
     public int getManager(int entryNum) {
         int manEntryNum = -1;
-        
+
         try {
             sql = "SELECT E2.entryNum FROM employee E1, employee E2"
                     + " WHERE E1.entryNum = " + entryNum + " AND E1.managerEntryNum = E2.entryNum";
             rs = stmt.executeQuery(sql);
 
-            if(rs.next())
+            if (rs.next()) {
                 manEntryNum = rs.getInt("entryNum");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -57,55 +58,58 @@ public class Database {
         // -- if returned is -1, manager is not found
         return manEntryNum;
     }
-    
-    public String getFirstName(int entryNum){
+
+    public String getFirstName(int entryNum) {
         String firstName = "";
 
         try {
             sql = "SELECT firstName FROM employee"
                     + " WHERE entryNum = " + entryNum;
             rs = stmt.executeQuery(sql);
-            
-            if(rs.next())
+
+            if (rs.next()) {
                 firstName = rs.getString("firstName");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return firstName;
     }
-    
-    public String getLastName(int entryNum){
+
+    public String getLastName(int entryNum) {
         String lastName = "";
 
         try {
             sql = "SELECT lastName FROM employee"
                     + " WHERE entryNum = " + entryNum;
             rs = stmt.executeQuery(sql);
-            
-            if(rs.next())
+
+            if (rs.next()) {
                 lastName = rs.getString("lastName");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return lastName;
     }
-    
-    public int getIDNumber(int entryNum){
+
+    public int getIDNumber(int entryNum) {
         int empID = 0;
 
         try {
             sql = "SELECT employeeID FROM employee"
                     + " WHERE entryNum = " + entryNum;
             rs = stmt.executeQuery(sql);
-            
-            if(rs.next())
+
+            if (rs.next()) {
                 empID = rs.getInt("employeeID");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return empID;
     }
-    
+
     public String getEmailAddress(int entryNum) {
         String emailAddress = "";
 
@@ -113,9 +117,8 @@ public class Database {
             sql = "SELECT emailAddress FROM employee"
                     + " WHERE entryNum = " + entryNum;
             rs = stmt.executeQuery(sql);
-            
-            if(rs.next())
-            {
+
+            if (rs.next()) {
                 emailAddress = rs.getString("emailAddress");
             }
         } catch (SQLException e) {
@@ -161,8 +164,8 @@ public class Database {
         }
         return isFound;
     }
-    
-    public int getEntryNum(int empID){
+
+    public int getEntryNum(int empID) {
         int entryNum = 0;
         sql = "SELECT entryNum FROM employee"
                 + " WHERE employeeID = " + empID;
@@ -176,57 +179,62 @@ public class Database {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        
+
         return entryNum;
     }
-    
-    public modelEmployee getEmployeeAccount(String username, String userpassword){
-        
-        sql = "SELECT * FROM employee" 
-            + " WHERE employeeID = " + username + " AND password = " + userpassword;
+
+    public modelEmployee getEmployeeAccount(String username, String userpassword) {
+
+        sql = "SELECT * FROM employee"
+                + " WHERE employeeID = " + username;
 
         try {
             rs = stmt.executeQuery(sql);
 
             if (rs.next()) {
-                int entryNum = rs.getInt("entryNum");
-                int employeeID = rs.getInt("employeeID");
-                String password = rs.getString("password");
-                String lastName = rs.getString("lastName");
-                String firstName = rs.getString("firstName");
-                String middleName = rs.getString("middleName");
-                String address = rs.getString("address");
-                Date birthday = rs.getDate("birthday");
-                String birthplace = rs.getString("birthplace");
-                long mobileNumber = rs.getLong("mobileNumber");
-                String SSSNumber = rs.getString("SSSNumber");
-                String TINNumber = rs.getString("TINNumber");
-                String PHSCNumber = rs.getString("PHSCNumber");
-                String PAGIBIGNumber = rs.getString("PAGIBIGNumber");
-                String civilStatus = rs.getString("civilStatus");
-                String citizenship = rs.getString("citizenship");
-                String religion = rs.getString("religion");
-                Date hireDate = rs.getDate("hireDate");
-                String band = rs.getString("band");
-                int salary = rs.getInt("salary");
-                int departmentID = rs.getInt("departmentID");
-                int positionID = rs.getInt("positionID");
-                boolean isDeleted = rs.getBoolean("isDeleted");
-                String emailAddress = rs.getString("emailAddress");
-                String employeeType = rs.getString("EmployeeType");
-                int managerEntryNum = rs.getInt("managerEntryNum");
-                
-                modelEmployee modelEmployee = new modelEmployee(entryNum, employeeID, lastName, 
-                                                                firstName, middleName,address, 
-                                                                birthday, birthplace, mobileNumber, 
-                                                                SSSNumber, TINNumber, PHSCNumber, 
-                                                                PAGIBIGNumber, civilStatus, citizenship, 
-                                                                religion, hireDate, band, salary,
-                                                                departmentID, positionID, isDeleted, 
-                                                                emailAddress, employeeType, managerEntryNum);
-                return modelEmployee;                                             
+                if (rs.getString("password").equals(userpassword)) {
+                    int entryNum = rs.getInt("entryNum");
+                    int employeeID = rs.getInt("employeeID");
+                    String password = rs.getString("password");
+                    String lastName = rs.getString("lastName");
+                    String firstName = rs.getString("firstName");
+                    String middleName = rs.getString("middleName");
+                    String address = rs.getString("address");
+                    Date birthday = rs.getDate("birthday");
+                    String birthplace = rs.getString("birthplace");
+                    long mobileNumber = rs.getLong("mobileNumber");
+                    String SSSNumber = rs.getString("SSSNumber");
+                    String TINNumber = rs.getString("TINNumber");
+                    String PHSCNumber = rs.getString("PHSCNumber");
+                    String PAGIBIGNumber = rs.getString("PAGIBIGNumber");
+                    String civilStatus = rs.getString("civilStatus");
+                    String citizenship = rs.getString("citizenship");
+                    String religion = rs.getString("religion");
+                    Date hireDate = rs.getDate("hireDate");
+                    String band = rs.getString("band");
+                    int salary = rs.getInt("salary");
+                    int departmentID = rs.getInt("departmentID");
+                    int positionID = rs.getInt("positionID");
+                    boolean isDeleted = rs.getBoolean("isDeleted");
+                    String emailAddress = rs.getString("emailAddress");
+                    String employeeType = rs.getString("EmployeeType");
+                    int managerEntryNum = rs.getInt("managerEntryNum");
+
+                    modelEmployee modelEmployee = new modelEmployee(entryNum, employeeID, lastName,
+                                                                    firstName, middleName, address,
+                                                                    birthday, birthplace, mobileNumber,
+                                                                    SSSNumber, TINNumber, PHSCNumber,
+                                                                    PAGIBIGNumber, civilStatus, citizenship,
+                                                                    religion, hireDate, band, salary,
+                                                                    departmentID, positionID, isDeleted,
+                                                                    emailAddress, employeeType, managerEntryNum);
+                    return modelEmployee;
+                }else{
+                    return null;
+                }
+            } else {
+                return null;
             }
-            else return null;
         } catch (SQLException e) {
             e.printStackTrace();
         }

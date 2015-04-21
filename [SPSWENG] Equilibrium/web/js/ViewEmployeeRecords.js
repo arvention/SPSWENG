d0dc8d3139e88edbe641fda3ff0dc34c9010dcd3
hsibayan <hannahsibayan@gmail.com>
2015-04-21 16:58:39 +0800

    US3/5-View and Edit Employee Records-04212015-0457
    US3/5-View and Edit Employee Records-04212015-0457

--- [SPSWENG] Equilibrium/web/js/ViewEmployeeRecords.js
+++ [SPSWENG] Equilibrium/web/js/ViewEmployeeRecords.js
@@ -1,13 +1,160 @@
 $(document).ready(function () {
     var currPage = "#personal";
     var nextPage;
+    var toBeDeleted;
     $(".pages").hide();
     $("#personal").show();
+    $("#alert").hide();
+    $(".add-delete-button").hide();
+    $("#overlay").hide();
 
-    $("#options").change(function () {
+    $(document).on("change", "#options", function () {
         nextPage = "#" + $("#options").find(":selected").val();
         $(currPage).hide();
         $(nextPage).show();
         currPage = nextPage;
     });
+    
+    $(document).on("click", "#editButton", function () {
+        if($(this).val() === "Edit") {
+            $(this).val("Save Edit");
+            $(".data").attr("contentEditable", true);
+            $(".data").css("border-bottom", "1px solid lightgray");
+            $(".add-delete-button").show();
+        }
+        else if($(this).val() === "Save Edit") {
+            $(this).val("Edit");
+            $(".data").attr("contentEditable", false);
+            $(".data").css("border-bottom", "0");
+            $(".add-delete-button").hide();
+        }
+    });
+    
+    $(document).on("click", ".add-delete-button", function () {
+        if($(this).val() === "+") {
+            var type = $(this).closest(".pages").attr("id");
+            var element;
+            
+            if($(this).parent().attr("class") === "label-rel")
+                element = $(this).parent(".label-rel");
+            else
+                element = $(this).closest(".subContent");
+            
+            console.log("+ BUTTON CLICKED");
+            console.log("   type: " + type);
+            console.log("   element: " + element);
+            
+            switch(type) {
+                case "relations": appendRelations(element); break;
+                case "edu": appendEducation(element); break;
+                case "history": appendHistory(element); 
+            }
+            reloadCss();
+            $(this).remove();
+        } 
+        else if($(this).val() === "-") {
+            toBeDeleted = $(this).closest(".subContent");
+            $("#overlay").fadeIn("fast");
+            $("#alert").fadeIn("fast");
+            $("#main :input").attr("disabled", true);
+        }
+    });
+    
+    $(document).on("click", ".alertButton", function () {
+        if($(this).val() === "Yes") {
+            console.log("SIBLING LEN " + toBeDeleted.siblings().length);
+            if(toBeDeleted.siblings().length <= 1)
+                $(toBeDeleted).siblings(".label-rel").append("<input type=\"button\" value=\"+\" class=\"add-delete-button\">");
+            toBeDeleted.remove();
+        } 
+        $("#alert").fadeOut("fast");
+        $("#overlay").fadeOut("fast");
+        $("#main :input").attr("disabled", false);
+    });
+    
+    function appendRelations(element) {
+        var content =
+            "<div class=\"subContent\">" +
+            "<div class=\"line\"><span class=\"label\"><b>Name</b></span>\n" +
+            "   <span class=\"data\"></span>\n" +
+            "   <input type=\"button\" value=\"+\" class=\"add-delete-button\">\n" +
+            "   <input type=\"button\" value=\"-\" class=\"add-delete-button\"></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Contact Number</span>\n" +
+            "   <span class=\"data\"></span></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Occupation</span>\n" +
+            "   <span class=\"data\"></span></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Employer</span>\n" +
+            "   <span class=\"data\"></span></div><br/>" +
+            "</div>";
+        $(element).after(content);
+    }
+    
+    function appendEducation(element) {
+        var content =
+            "<div class=\"subContent\">" +
+            "<div class=\"line\"><span class=\"label\"><b>Name</b></span>\n" +
+            "   <span class=\"data\"></span>\n" +
+            "   <input type=\"button\" value=\"+\" class=\"add-delete-button\">\n" +
+            "   <input type=\"button\" value=\"-\" class=\"add-delete-button\"></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Age</span>\n" +
+            "   <span class=\"data\"></span></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Occupation</span>\n" +
+            "   <span class=\"data\"></span></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Company</span>\n" +
+            "   <span class=\"data\"></span></div>" +
+            "</div>";
+        $(element).after(content);
+    }
+    
+    function appendHistory(element) {
+        var content =
+            "<div class=\"subContent\">" +
+            "<div class=\"label-rel\">Year</div>" +
+            "<div class=\"line\"><span class=\"label\"><b>Occupation</b></span>\n" +
+            "   <span class=\"data\"></span>\n" +
+            "   <input type=\"button\" value=\"+\" class=\"add-delete-button\">\n" +
+            "   <input type=\"button\" value=\"-\" class=\"add-delete-button\"></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Date of Employment</span>\n" +
+            "   <span class=\"data\"></span></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Beginning Salary</span>\n" +
+            "   <span class=\"data\"></span></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Ending Salary</span>\n" +
+            "   <span class=\"data\"></span></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Name of Employer</span>\n" +
+            "   <span class=\"data\"></span></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Address of Employer</span>\n" +
+            "   <span class=\"data\"></span></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Contact Number of Employer</span>\n" +
+            "   <span class=\"data\"></span></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Name of Supervisor</span>\n" +
+            "   <span class=\"data\"></span></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Contact Number of Supervisor</span>\n" +
+            "   <span class=\"data\"></span></div>\n" +
+            "<div class=\"line\"><span class=\"label\">Reason for leaving</span>\n" +
+            "   <span class=\"data\"></span></div>" + 
+            "</div>";
+        $(element).after(content);
+    }
+    
+    function reloadCss() {
+        $(".add-delete-button").css(button);
+        $(".data").css(data);
+        $(".data").attr("contentEditable", true);
+        $(".data").css("border-bottom", "1px solid lightgray");
+    }
+    
+    var button = {
+        'width': '20px',
+        'float': 'right',
+        'background-color': 'lightgray',
+        'border': '0',
+        'color': 'white',
+        'margin-left': '2px'
+    };
+    
+    var data = {
+        'padding-left': '2px',
+        'padding-right': '2px'
+    };
+    
 });

c95cf77406e8c6b4d69a802607c1f5893d8522f8
angpulangpluma <mariennelopez30@gmail.com>
2015-04-24 16:40:58 +0800

    whatever
    Homepage-HrHead.js

--- [SPSWENG] Equilibrium/web/js/Homepage-HrHead.js
+++
@@ -1,9 +0,0 @@
-function accept(leaveFirstName, leaveLastName) {
-    alert("You accepted " + leaveFirstName + " " + leaveLastName + "'s leave.");
-    return true;
-}
-
-function reject(leaveFirstName, leaveLastName) {
-    alert("You rejected " + leaveFirstName + " " + leaveLastName + "'s leave.");
-    return true;
-}

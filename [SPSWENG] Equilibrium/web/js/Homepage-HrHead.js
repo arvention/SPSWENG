b9cf9072fd3371e36ec32204d0a4d19aa8525feb
arvention <amc.antioquia@yahoo.com.ph>
2015-04-24 16:57:15 +0800

    Revert "whatever"
    This reverts commit c95cf77406e8c6b4d69a802607c1f5893d8522f8.

---
+++ [SPSWENG] Equilibrium/web/js/Homepage-HrHead.js
@@ -0,0 +1,9 @@
+function accept(leaveFirstName, leaveLastName) {
+    alert("You accepted " + leaveFirstName + " " + leaveLastName + "'s leave.");
+    return true;
+}
+
+function reject(leaveFirstName, leaveLastName) {
+    alert("You rejected " + leaveFirstName + " " + leaveLastName + "'s leave.");
+    return true;
+}

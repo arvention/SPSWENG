e43b6682efe4a8a603887f6c8aac6e526152d105
arvention <amc.antioquia@yahoo.com.ph>
2015-04-27 00:17:59 +0800

    Coding Convention Fix fro GetImage

--- [SPSWENG] Equilibrium/src/java/Servlets/GetImage.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/GetImage.java
@@ -1,143 +1,82 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Servlets;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Thursday
- */
 public class GetImage extends HttpServlet {
 
-    private static final int BUFFER_SIZE = 4096;   
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet GetImage</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet GetImage at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+    private static final int BUFFER_SIZE = 4096;
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doGet function of the servlet.
+    */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
-        
+
         /*
-        byte[] imageData = Database.Database.getInstance().getImage(13);
-        response.setContentType("image/jpeg");
-        response.getOutputStream().write(imageData);
-        */
-        
+         byte[] imageData = Database.Database.getInstance().getImage(13);
+         response.setContentType("image/jpeg");
+         response.getOutputStream().write(imageData);
+         */
         String id = request.getParameter("id");
         int intid = Integer.parseInt(id.trim());
-        
-        String theFilename="download";
-        StringBuilder fname = new StringBuilder ();
-        InputStream is = Database.Database.getInstance().getInputStream(intid,fname);
+
+        String theFilename = "download";
+        StringBuilder fname = new StringBuilder();
+        InputStream is = Database.Database.getInstance().getInputStream(intid, fname);
         String filename = fname.toString();
-        System.out.println("File name is "+ filename);
-        theFilename=theFilename + filename.substring(0, filename.lastIndexOf('.'));
+        System.out.println("File name is " + filename);
+        theFilename = theFilename + filename.substring(0, filename.lastIndexOf('.'));
         int fileLength = is.available();
-                 
+
         System.out.println("fileLength = " + fileLength);
- 
-                ServletContext context = getServletContext();
- 
-                // sets MIME type for the file download
-                
-                String mimeType = context.getMimeType(filename);
-                if (mimeType == null) {        
-                    mimeType = "application/octet-stream";
-                }          
-        
-                response.setContentType(mimeType);
-                response.setContentLength(fileLength);
-                String headerKey = "Content-Disposition";
-                String headerValue = String.format("attachment; filename=\"%s\"",filename);
-                response.setHeader(headerKey, headerValue);
- 
-                // writes the file to the client
-                OutputStream outStream = response.getOutputStream();
-                 
-                byte[] buffer = new byte[BUFFER_SIZE];
-                int bytesRead = -1;
-                 
-                while ((bytesRead = is.read(buffer)) != -1) {
-                    outStream.write(buffer, 0, bytesRead);
-                }
-                 
-                is.close();
-                outStream.close();                
+
+        ServletContext context = getServletContext();
+
+        // sets MIME type for the file download
+        String mimeType = context.getMimeType(filename);
+        if (mimeType == null) {
+            mimeType = "application/octet-stream";
+        }
+
+        response.setContentType(mimeType);
+        response.setContentLength(fileLength);
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=\"%s\"", filename);
+        response.setHeader(headerKey, headerValue);
+
+        // writes the file to the client
+        OutputStream outStream = response.getOutputStream();
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytesRead = -1;
+
+        while ((bytesRead = is.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+
+        is.close();
+        outStream.close();
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doPost function of the servlet.
+    */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-       // String imageId = request.getParameter("imageId");
+
+        // String imageId = request.getParameter("imageId");
         byte[] imageData = Database.Database.getInstance().getImage(13);
         response.setContentType("image/jpeg");
         response.getOutputStream().write(imageData);
-        
+
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }

ab9bcb39131d8a9fd1ab122ab8404528aeb7eb34
arvention <amc.antioquia@yahoo.com.ph>
2015-04-26 21:43:21 +0800

    Coding Convention Fix on DisplayImage servlet

--- [SPSWENG] Equilibrium/src/java/Servlets/DisplayImage.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/DisplayImage.java
@@ -1,9 +1,8 @@
 
 /*  Class Name:     DisplayImage
-*   Developer:      JetVirtusio
-*   Description:    servlet used to display the image of each employee
-*/  
-
+ *   Developer:      JetVirtusio
+ *   Description:    servlet used to display the image of each employee
+ */
 package Servlets;
 
 import java.io.IOException;
@@ -15,25 +14,25 @@ import javax.servlet.http.HttpServletResponse;
 public class DisplayImage extends HttpServlet {
 
     /*
-    *   The doGet function of the servlet.
-    */
+     *   The doGet function of the servlet.
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-          
+
         String id = request.getParameter("id");
         int intid = Integer.parseInt(id.trim());
         byte[] imageData = Database.Database.getInstance().getImage(intid);
-        
-        if (imageData != null){
+
+        if (imageData != null) {
             response.setContentType("image/jpeg");
             response.getOutputStream().write(imageData);
         }
     }
 
     /*
-    *   The doPost function of the servlet.
-    */
+     *   The doPost function of the servlet.
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {

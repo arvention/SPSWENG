a7d2bd2295706d898ae0bc89f9d2a01edff08054
arvention <amc.antioquia@yahoo.com.ph>
2015-04-27 00:44:19 +0800

    Coding convention Fix for LeaveSubmit

--- [SPSWENG] Equilibrium/src/java/Servlets/LeaveSubmit.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/LeaveSubmit.java
@@ -1,18 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/*  Class Name:     LeaveSubmit
+*   Developer:      Arces Talavera
+*   Description:    servlet used by the system to submit the leaves to the database
+*/
+
 package Servlets;
 
 import Database.Database;
 import ClassHelpers.EmailNotifier;
 import Models.modelEmployee;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -21,63 +20,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Arces
- */
 public class LeaveSubmit extends HttpServlet {
 
     private final int maxLeave = 15;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LeaveSubmit</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LeaveSubmit at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doGet function of the servlet.
+    */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doPost function of the servlet.
+    *   Updates changes in the database using queries.
+    */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -110,25 +68,14 @@ public class LeaveSubmit extends HttpServlet {
             } else {
                 String leaveType = request.getParameter("leaveType");
                 float duration = Float.parseFloat(request.getParameter("dayCount"));
-                
+
                 db.addLeaveForm(leaveType, modelEmployee.getEntryNum(), new java.sql.Date(startDate.getTime()), duration, modelEmployee.getManagerEntryNum());
 
                 //send an email to the manager
-             //   en.sendLeaveRequest(modelEmployee.getEntryNum(), leaveType, startDate, endDate, duration);
+                //en.sendLeaveRequest(modelEmployee.getEntryNum(), leaveType, startDate, endDate, duration);
             }
         }
-
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
     // -- function/s ----------------------------------------
     public int getDateDiff(Date startDate, Date endDate) {

ae13751d6dcf57ac70bc64da8ae2d9baf5f16eb4
arvention <amc.antioquia@yahoo.com.ph>
2015-04-26 21:37:21 +0800

    Coding Convention Fix on DisplayImage servlet

--- [SPSWENG] Equilibrium/src/java/Servlets/DisplayImage.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/DisplayImage.java
@@ -1,59 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/*  Class Name:     DisplayImage
+*   Developer:      JetVirtusio
+*   Description:    servlet used to display the image of each employee
+*/  
 
 package Servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Thursday
- */
 public class DisplayImage extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet DisplayImage</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet DisplayImage at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doGet function of the servlet.
+    */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -61,38 +24,18 @@ public class DisplayImage extends HttpServlet {
         String id = request.getParameter("id");
         int intid = Integer.parseInt(id.trim());
         byte[] imageData = Database.Database.getInstance().getImage(intid);
-        if(imageData != null){
-        response.setContentType("image/jpeg");
-        response.getOutputStream().write(imageData);
+        
+        if (imageData != null){
+            response.setContentType("image/jpeg");
+            response.getOutputStream().write(imageData);
         }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doPost function of the servlet.
+    */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-             
-        
-
- 
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }

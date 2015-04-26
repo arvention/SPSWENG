327bf6e268ca6b4174b58805a146a0bab9aa55a2
arvention <amc.antioquia@yahoo.com.ph>
2015-04-26 22:02:44 +0800

    Coding Convention Fix for EditEmployeeData servlet

--- [SPSWENG] Equilibrium/src/java/Servlets/EditEmployeeData.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/EditEmployeeData.java
@@ -1,74 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/*  Class Name:     EditEmployeeData
+*   Developer:      Arces Talavera
+*   Description:    servlet used by the hr manager to submit the edited employee data
+*/ 
+
 package Servlets;
 
 import Database.Database;
 import Models.modelEmployee;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Arces
- */
+
 public class EditEmployeeData extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet EditEmployeeData</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet EditEmployeeData at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doGet function of the servlet.
+    */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doPost function of the servlet.
+    */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -76,16 +35,6 @@ public class EditEmployeeData extends HttpServlet {
         modelEmployee emp = (modelEmployee) request.getSession().getAttribute("viewEmp");
         editPersonalInformation(logged, emp, request);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
     public void editPersonalInformation(modelEmployee logged, modelEmployee emp, HttpServletRequest request) {
         Database db = Database.getInstance();

b224aa56107e24a05215cbc3d9d9ae199a2e611a
arvention <amc.antioquia@yahoo.com.ph>
2015-04-26 22:33:08 +0800

    Coding Convention Fix for GetEmployeeOfManagerServlet

--- [SPSWENG] Equilibrium/src/java/Servlets/GetEmployeeOfManagerServlet.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/GetEmployeeOfManagerServlet.java
@@ -1,15 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/*  Class Name:     GetEmployeeOfManagerServlet
+*   Developer:      Jet Virtusio
+*   Description:    servlet used to get employees of a manager account.
+*/  
 
 package Servlets;
 
 import Database.Database;
 import Models.modelEmployee;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,55 +16,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Thursday
- */
 public class GetEmployeeOfManagerServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet GetEmployeeOfManagerServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet GetEmployeeOfManagerServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doGet function of the servlet.
+    */  
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
         HttpSession session = request.getSession();
         modelEmployee emp = (modelEmployee) session.getAttribute("employee");
-        System.out.println("DUDE DA ID IS "+ emp.getEmployeeID());
-        
         
         Database db = Database.getInstance();
         ArrayList<modelEmployee> employees = db.getEmployeeOfManager(emp.getEntryNum());
@@ -74,36 +35,14 @@ public class GetEmployeeOfManagerServlet extends HttpServlet {
         response.sendRedirect("FileMemo.jsp");
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doPost function of the servlet.
+    *   Updates changes in the database using queries.
+    */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        int entrynum=1;
-        
-        
-        
-        
-        
-        
-        
+        int entrynum = 1; 
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }

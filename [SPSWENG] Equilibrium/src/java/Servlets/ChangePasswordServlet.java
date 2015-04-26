e073731841d296c66c561303b9663a7f9b277cfb
arvention <amc.antioquia@yahoo.com.ph>
2015-04-26 21:25:48 +0800

    Coding Convention fix for ChangePasswordServlet

--- [SPSWENG] Equilibrium/src/java/Servlets/ChangePasswordServlet.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/ChangePasswordServlet.java
@@ -1,15 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/*  Class Name:     ChangePasswordServlet
+*   Developer:      Jet Virtusio
+*   Description:    servlet used to change password of any employee type
+*/  
 
 package Servlets;
 
 import Database.Database;
 import Models.modelEmployee;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,61 +16,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Thursday
- */
 public class ChangePasswordServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ChangePasswordServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ChangePasswordServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -81,30 +32,18 @@ public class ChangePasswordServlet extends HttpServlet {
         RequestDispatcher view;
         HttpSession session = request.getSession();
         modelEmployee me;
-        me =(modelEmployee) session.getAttribute("employee");
-        if(Database.getInstance().isUserPassword( me.getEmployeeID(),oldpass)){
+        me = (modelEmployee) session.getAttribute("employee");
+        
+        if (Database.getInstance().isUserPassword(me.getEmployeeID(),oldpass)){
             Database.getInstance().changePassword(me.getEntryNum(), newpass);
             //request.setAttribute("error", "Success! Password Changed");
             view = request.getRequestDispatcher("ChangePasswordSuccess.jsp");
-        }else{
-         request.setAttribute("error", "Invalid Password");
-         //request.setAttribute("error", "Invalid Username/Password");
+        } else {
+            request.setAttribute("error", "Invalid Password");
+            //request.setAttribute("error", "Invalid Username/Password");
           view = request.getRequestDispatcher("changePassword.jsp");
         }
         
-          // view = request.getRequestDispatcher("ChangePasswordSuccess.jsp");
-           view.forward(request, response);
-            
+        view.forward(request, response);    
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }

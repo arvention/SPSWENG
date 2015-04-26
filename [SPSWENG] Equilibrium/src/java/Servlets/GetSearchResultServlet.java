52c2a5197efd27e18fec04d43ecbc3a1ebaebfc5
arvention <amc.antioquia@yahoo.com.ph>
2015-04-27 00:36:30 +0800

    Coding Convention for GetSearchResultServlet

--- [SPSWENG] Equilibrium/src/java/Servlets/GetSearchResultServlet.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/GetSearchResultServlet.java
@@ -1,16 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
+/*   Class Name:     GetSearchResultServlet
+ *   Developer:      Arren Antioquia
+ *   Description:    servlet used to generate search result
  */
-
 package Servlets;
-
 
 import Database.Database;
 import Models.modelEmployee;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,83 +15,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Thursday
- */
 public class GetSearchResultServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet createAccountServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet createAccountServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+    /*
+     *   The doGet function of the servlet.
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+    /*
+     *   The doPost function of the servlet.
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-          String search = request.getParameter("search");
-          Database db = Database.getInstance();
-          
-          ArrayList<modelEmployee> modelEmployees = db.getSearchResult(search);
-          HttpSession session= request.getSession();
-          session.setAttribute("searchResult",modelEmployees);
-          response.sendRedirect("SearchEmp.jsp");
-        
+
+        String search = request.getParameter("search");
+        Database db = Database.getInstance();
+
+        ArrayList<modelEmployee> modelEmployees = db.getSearchResult(search);
+        HttpSession session = request.getSession();
+        session.setAttribute("searchResult", modelEmployees);
+        response.sendRedirect("SearchEmp.jsp");
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }

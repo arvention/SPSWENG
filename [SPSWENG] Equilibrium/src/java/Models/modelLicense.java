3826f6da3ad9af49cb7869ef30449a622d82b027
arvention <amc.antioquia@yahoo.com.ph>
2015-04-26 16:33:13 +0800

    Coding Convention Fix for modelLicense class

--- [SPSWENG] Equilibrium/src/java/Models/modelLicense.java
+++ [SPSWENG] Equilibrium/src/java/Models/modelLicense.java
@@ -1,40 +1,70 @@
 
+/*  Class Name:     modelLicense
+*   Developer:      Arren Antioquia
+*   Description:    model for the license table in the database
+*/ 
+
 package Models;
 
 public class modelLicense {
+    
     private int licenseID;
     private String licenseName;
     private int empEntryNum;
     private int percentage;
 
+    /*
+    *   getter function for licenseID
+    */
     public int getLicenseID() {
         return licenseID;
     }
 
+    /*
+    *   setter function for licenseID
+    */
     public void setLicenseID(int licenseID) {
         this.licenseID = licenseID;
     }
 
+    /*
+    *   getter function for licenseName
+    */
     public String getLicenseName() {
         return licenseName;
     }
 
+    /*
+    *   setter function for licenseName
+    */
     public void setLicenseName(String licenseName) {
         this.licenseName = licenseName;
     }
 
+    /*
+    *   getter function for empEntryNum
+    */
     public int getEmpEntryNum() {
         return empEntryNum;
     }
 
+    /*
+    *   setter function for empEntryNum
+    */
     public void setEmpEntryNum(int empEntryNum) {
         this.empEntryNum = empEntryNum;
     }
 
+    /*
+    *   getter function for percentage
+    */
     public int getPercentage() {
         return percentage;
     }
 
+    /*
+    *   setter function for percentage
+    */
     public void setPercentage(int percentage) {
         this.percentage = percentage;
     }

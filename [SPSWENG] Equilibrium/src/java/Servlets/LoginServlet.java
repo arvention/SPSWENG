292d875c92bc7952c508a1e6b550f7e141fef772
arvention <amc.antioquia@yahoo.com.ph>
2015-04-27 00:58:34 +0800

    Coding Convention Fix for Login Servlet

--- [SPSWENG] Equilibrium/src/java/Servlets/LoginServlet.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/LoginServlet.java
@@ -1,8 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/*  Class Name:     LoginServlet
+*   Developer:      Arren Antioquia
+*   Description:    servlet used to login to the system
+*/  
+
 package Servlets;
 
 import Database.Database;
@@ -16,120 +17,71 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Arren Antioquia
- */
 public class LoginServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doGet function of the servlet.
+    */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doPost function of the servlet.
+    *   checks the database if the username and the password match for an actual account
+    */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
-        System.out.println("inside log in servlet");
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         
+        /*  gets request object */
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         Database database = Database.getInstance();
         
+        /*  gets session    */
         HttpSession session = request.getSession();
         
+        /*  get Employee Account, returns null if the account does not exists*/
         modelEmployee modelEmployee = database.getEmployeeAccount(username, password);
         
-        
-        if(modelEmployee != null){
-            if(modelEmployee.getEmployeeType().equalsIgnoreCase("Employee")){
+        /*  if account exists, go to proper homepage    */
+        if (modelEmployee != null){
+            if (modelEmployee.getEmployeeType().equalsIgnoreCase("Employee")){
                 session.setAttribute("employee", modelEmployee);
                 RequestDispatcher view = request.getRequestDispatcher("Homepage-Employee.jsp");
                 view.forward(request, response);
             }
-            else if(modelEmployee.getEmployeeType().equalsIgnoreCase("Hr Head")){
+            else if (modelEmployee.getEmployeeType().equalsIgnoreCase("Hr Head")){
                 session.setAttribute("employee", modelEmployee);
                 RequestDispatcher view = request.getRequestDispatcher("Homepage-HrHead.jsp");
                 view.forward(request, response);
             }
-            else if(modelEmployee.getEmployeeType().equalsIgnoreCase("Hr Employee")){
+            else if (modelEmployee.getEmployeeType().equalsIgnoreCase("Hr Employee")){
                 session.setAttribute("employee", modelEmployee);
                 RequestDispatcher view = request.getRequestDispatcher("Homepage-HrEmployee.jsp");
                 view.forward(request, response);
             }
-            else if(modelEmployee.getEmployeeType().equalsIgnoreCase("Manager")){
+            else if (modelEmployee.getEmployeeType().equalsIgnoreCase("Manager")){
                 session.setAttribute("employee", modelEmployee);
                 RequestDispatcher view = request.getRequestDispatcher("Homepage-Manager.jsp");
                 view.forward(request, response);
             }
-            else if(modelEmployee.getEmployeeType().equalsIgnoreCase("Senior Manager")){
+            else if (modelEmployee.getEmployeeType().equalsIgnoreCase("Senior Manager")){
                 session.setAttribute("employee", modelEmployee);
                 RequestDispatcher view = request.getRequestDispatcher("Homepage-SManager.jsp");
                 view.forward(request, response);
             }
         }
+        
+        /*  else show error, and go back to the log in page */
         else{
             request.setAttribute("error", "Invalid Username/Password");
             RequestDispatcher view = request.getRequestDispatcher("index.jsp");
             view.forward(request, response);
         }
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }

6aa07a9e97c1d8c8c6ffc9197d48b075b87045d4
arvention <amc.antioquia@yahoo.com.ph>
2015-04-26 13:01:38 +0800

    Coding Convention Fix for modelBranch class

--- [SPSWENG] Equilibrium/src/java/Models/modelBranch.java
+++ [SPSWENG] Equilibrium/src/java/Models/modelBranch.java
@@ -1,37 +1,64 @@
+
+/*  Class Name:     modelBranch
+*   Developer:      Arren Antioquia
+*   Description:    Objects that can be downloaded through the system.
+*/  
+
 package Models;
 
-
 public class modelBranch {
+    
     private int branchID;
     private String name;
     private String address;
     
+    /*
+    *   constructor with no parameter
+    */
     public modelBranch(int branchID, String name, String address){
         this.branchID = branchID;
         this.name = name;
         this.address = address;
     }
     
+    /*
+    *   getter function for branchID
+    */
     public int getBranchID() {
         return branchID;
     }
-
+    
+    /*
+    *   setter function for branchID
+    */
     public void setBranchID(int branchID) {
         this.branchID = branchID;
     }
 
+    /*
+    *   getter function for name
+    */
     public String getName() {
         return name;
     }
 
+    /*
+    *   setter function for name
+    */
     public void setName(String name) {
         this.name = name;
     }
 
+    /*
+    *   getter function for address
+    */
     public String getAddress() {
         return address;
     }
 
+    /*
+    *   setter function for address
+    */
     public void setAddress(String address) {
         this.address = address;
     }

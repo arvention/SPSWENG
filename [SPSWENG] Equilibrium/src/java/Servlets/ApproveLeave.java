074f58e85b76dde43439cf7f747445f4d2a32a02
arvention <amc.antioquia@yahoo.com.ph>
2015-04-26 21:16:07 +0800

    Coding Convention fix for ApproveLeave servlet

--- [SPSWENG] Equilibrium/src/java/Servlets/ApproveLeave.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/ApproveLeave.java
@@ -1,102 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/*  Class Name:     ApproveLeave
+*   Developer:      Arren Antioquia
+*   Description:    servlet used by the hr head to approve a leave of his employee
+*/  
+
 package Servlets;
 
 import Database.Database;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Arren Antioquia
- */
 public class ApproveLeave extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ApproveLeave</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ApproveLeave at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doGet function of the servlet.
+    */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doPost function of the servlet.
+    *   Updates changes in the database using queries.
+    */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        /*  gets instance of the database   */
         Database db = Database.getInstance();
         
+        /*  gets the parameters passed to the request object    */
         int leaveID = Integer.parseInt(request.getParameter("leaveID"));
         String approveValue = request.getParameter("approveValue");
         String curpage = request.getParameter("curpage");
         
+        /*  changes the leave status to the value passed to the request object  */
         db.changeLeaveStatus(leaveID, approveValue);
         
-        if(curpage.equals("homepage-manager")){
+        /*  redirects to the page   */
+        if (curpage.equals("homepage-manager")){
             RequestDispatcher view = request.getRequestDispatcher("Homepage-Manager.jsp");
             view.forward(request, response);
+        } else if (curpage.equals("homepage-smanager")){
+            RequestDispatcher view = request.getRequestDispatcher("Homepage-SManager.jsp");
+            view.forward(request, response);
         }
-        else if(curpage.equals("homepage-hrhead")){
+        else if (curpage.equals("homepage-hrhead")){
             RequestDispatcher view = request.getRequestDispatcher("Homepage-HrHead.jsp");
             view.forward(request, response);
         }
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }

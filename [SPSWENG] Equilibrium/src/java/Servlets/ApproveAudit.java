74c612cf3c12258524ee2c3638648651847b2af9
arvention <amc.antioquia@yahoo.com.ph>
2015-04-26 18:48:12 +0800

    Coding Convention fix for ApproveAudit servlet

--- [SPSWENG] Equilibrium/src/java/Servlets/ApproveAudit.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/ApproveAudit.java
@@ -1,103 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/*  Class Name:     ApproveAudit
+*   Developer:      Arren Antioquia
+*   Description:    servlet used by the hr head to approve an employee audit trail entry
+*/  
+
 package Servlets;
 
 import Database.Database;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Arren Antioquia
- */
 public class ApproveAudit extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ApproveAudit</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ApproveAudit at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doGet function of the servlet.
+    */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doPost function of the servlet.
+    *   Updates changes in the database using queries.
+    */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        /*  gets the database instance and the parameters from the request object   */
         Database db = Database.getInstance();
         int empAuditTrailID = Integer.parseInt(request.getParameter("empAuditTrailID"));
         String approveValue = request.getParameter("approveValue");
         
+        /*  changes the audit status base on the value passed to the servlet    */
         db.changeAuditStatus(empAuditTrailID, approveValue);
         
-        String tableName = request.getParameter("tableName");
-        int tableReferenceNum = Integer.parseInt(request.getParameter("tableReferenceNum"));
-        String field = request.getParameter("field");
-        Object value = request.getParameter("value");
-        
-        if(approveValue.equals("Approved"))
+        /*  if the audit trail is approved, change the value in the specified table */
+        if (approveValue.equals("Approved")){
+            /*  gets other parameters passed to the request object for changing values in the database  */
+            String tableName = request.getParameter("tableName");
+            int tableReferenceNum = Integer.parseInt(request.getParameter("tableReferenceNum"));
+            String field = request.getParameter("field");
+            Object value = request.getParameter("value");
+            
+            /*  changes the value in the field on the specified table   */
             db.changeFieldValue(tableName, tableReferenceNum, field, value);
+        }
         
+        /*  redirects back to homepage   */
         RequestDispatcher view = request.getRequestDispatcher("Homepage-HrHead.jsp");
         view.forward(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }

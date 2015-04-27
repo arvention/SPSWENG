35214a01e24ece0afa71b42b4a49adbfc89e68c6
arvention <amc.antioquia@yahoo.com.ph>
2015-04-27 09:37:33 +0800

    Coding Convention for SaveProfilePicture

--- [SPSWENG] Equilibrium/src/java/Servlets/SaveProfilePic.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/SaveProfilePic.java
@@ -1,16 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/*  Class Name:     SaveProfPic
+*   Developer:      Jet Virtusio
+*   Description:    servlet used to save the profile pic to the database
+*/  
+
 package Servlets;
 
 import Database.Database;
-import Models.modelEmployee;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
@@ -18,84 +16,53 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-    /*    
-    Classname:   SaveProfilePic
-    Developer:   Jet Virtusio
-    Description: Saves Picture in datavase
-    */
-
-
-
-@MultipartConfig(maxFileSize = 16177215)  
+@MultipartConfig(maxFileSize = 16177215)
 public class SaveProfilePic extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doGet function of the servlet.
+    */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doPost function of the servlet.
+    *   Updates changes in the database using queries.
+    */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-    
-        Part filePart = request.getPart("filename");
-        
-        System.out.println("Heree");
-        
-        String id = request.getParameter("id");
-         int intid = Integer.parseInt(id.trim());
-         InputStream inputStream = filePart.getInputStream();
-        String name = filePart.getSubmittedFileName();
-        
-        String reply="0"; 
-        
-        try{
-      if(filePart.getSize() !=0 ){
-      Database.getInstance().savePic(intid,inputStream, filePart.getSubmittedFileName());
-      
-      }else reply ="2";
 
-        }catch(Exception e){
+        Part filePart = request.getPart("filename");
+
+        System.out.println("Heree");
+
+        String id = request.getParameter("id");
+        int intid = Integer.parseInt(id.trim());
+        InputStream inputStream = filePart.getInputStream();
+        String name = filePart.getSubmittedFileName();
+
+        String reply = "0";
+
+        try {
+            if (filePart.getSize() != 0) {
+                Database.getInstance().savePic(intid, inputStream, filePart.getSubmittedFileName());
+
+            } else {
+                reply = "2";
+            }
+
+        } catch (Exception e) {
             reply = "1";
             System.out.println(e);
         }
-      
-      response.setContentType("text/plain");  
-      response.setCharacterEncoding("UTF-8"); 
-      response.getWriter().write(reply); 
-      
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(reply);
+
     // response.sendRedirect("ViewEmployeeRecords.jsp");
-        
-   
     }
-
-
 }

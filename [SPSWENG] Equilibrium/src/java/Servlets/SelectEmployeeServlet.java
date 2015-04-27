b80ee4d9be2d62b2e88daf7c773181a4995367d8
arvention <amc.antioquia@yahoo.com.ph>
2015-04-27 09:48:20 +0800

    Coding Convention for SelectEmployeeServlet

--- [SPSWENG] Equilibrium/src/java/Servlets/SelectEmployeeServlet.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/SelectEmployeeServlet.java
@@ -1,111 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
-goes here when the employee clicks on search result.
 
-*/
+/*  Class Name:     SelectEmployeeServlet
+*   Developer:      Jet Virtusio
+*   Description:    Redirects a user to the function clicked in the search page
+*/  
 
 package Servlets;
 
 import Database.Database;
 import Models.modelEmployee;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Thursday
- */
-
-    /*
-    
-    Classname:   SearchEmployeeServlet
-    Developer:   Jet Virtusio
-    Description: Redirects a user to the function clicked in the search page
-    */
-
-
 public class SelectEmployeeServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SelectEmployeeServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SelectEmployeeServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doGet function of the servlet.
+    */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {       
+            throws ServletException, IOException {
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doPost function of the servlet.
+    */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        
-       
+
         String id = request.getParameter("idnumber");
         modelEmployee employee = Database.getInstance().getEmployee(id);
-        
-        System.out.println("ID OF EMPLOYEE IS "+id );
-        
+
+        System.out.println("ID OF EMPLOYEE IS " + id);
+
         HttpSession session = request.getSession();
         session.setAttribute("selectedemployee", employee);
-        
-        
-        if(request.getParameter("type").equals("file")){
-        response.sendRedirect("FileMemo.jsp");    
-        }else if(request.getParameter("type").equals("award")){     
+
+        if (request.getParameter("type").equals("file")) {
+            response.sendRedirect("FileMemo.jsp");
+        } else if (request.getParameter("type").equals("award")) {
             response.sendRedirect("addAward.jsp");
-        }else if(request.getParameter("type").equals("evaluate")){
+        } else if (request.getParameter("type").equals("evaluate")) {
             response.sendRedirect("addEvaluation.jsp");
         }
-        
-        
     }
-
- 
 }

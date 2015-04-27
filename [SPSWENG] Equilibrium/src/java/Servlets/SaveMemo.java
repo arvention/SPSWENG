5a0e233ba2ad83b663052eec64b17e4691286c70
arvention <amc.antioquia@yahoo.com.ph>
2015-04-27 09:05:47 +0800

    Coding Convention for Save Memo servlet

--- [SPSWENG] Equilibrium/src/java/Servlets/SaveMemo.java
+++ [SPSWENG] Equilibrium/src/java/Servlets/SaveMemo.java
@@ -1,19 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
+/*  Class Name:     SaveMemo
+*   Developer:      Jet Virtusio
+*   Description:    servlet used to save the memo in the database
+*/  
 package Servlets;
 
 import Database.Database;
-import ClassHelpers.EmailNotifier;
 import Models.modelEmployee;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
@@ -22,132 +17,69 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
-/**
- *
- * @author Thursday
- */
-
-/*  Class Name:     SaveMemo.java
-*   Developer:      Jet Virtusio
-*   Description:    Saves Memo in database
-*/  
-
-@MultipartConfig(maxFileSize = 16177215)  
+@MultipartConfig(maxFileSize = 16177215)
 public class SaveMemo extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SaveMemo</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SaveMemo at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doGet function of the servlet.
+    */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    /*
+    *   The doPost function of the servlet.
+    *   Updates changes in the database using queries.
+    */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
         modelEmployee employee = (modelEmployee) session.getAttribute("selectedemployee");
         Part filePart = request.getPart("filename");
-        boolean flag= true;
-        
+        boolean flag = true;
+
         String typeofmemo = request.getParameter("listTypeMemo");
         int intid = employee.getEntryNum();
         String memo = request.getParameter("memoNote");
         InputStream inputStream = null;
-        
+
         if (memo.length() >= 2500) {
             System.out.println("Oh no too much characters");
             session.setAttribute("error", new String("* Max characters reached."));
             //response.sendRedirect("FileMemo.jsp");
-            flag=false;
+            flag = false;
         }
-        if(filePart.getSize() != 0){
-            
-            System.out.println("File Size is "+ filePart.getSize());
-            if(filePart.getSize() >  10847412){
+        if (filePart.getSize() != 0) {
+
+            System.out.println("File Size is " + filePart.getSize());
+            if (filePart.getSize() > 10847412) {
                 session.setAttribute("error", new String("* Max File Size Reached."));
-               // response.sendRedirect("FileMemo.jsp");
-                flag=false;
+                // response.sendRedirect("FileMemo.jsp");
+                flag = false;
             }
-            
-            
+
         }
-        
-        if(!flag){
+
+        if (!flag) {
             System.out.println("Redirect now");
-            response.sendRedirect("FileMemo.jsp"); 
-        }else{
-           
-            
-           Database db = Database.getInstance();
-           if(filePart.getSize() != 0){
-             
-             inputStream = filePart.getInputStream();
-             System.out.println("I am over gereee");  
-             String nameoffile =  filePart.getSubmittedFileName();
-             db.saveDisciplinary(intid, memo,typeofmemo,inputStream,nameoffile );  
-           }
-           else{
-               db.saveDisciplinary(intid, memo,typeofmemo,null,null);    
-                 // response.sendRedirect("FileMemoSuccess.jsp");
-           }
-           session.removeAttribute("error");
+            response.sendRedirect("FileMemo.jsp");
+        } else {
+
+            Database db = Database.getInstance();
+            if (filePart.getSize() != 0) {
+
+                inputStream = filePart.getInputStream();
+                System.out.println("I am over gereee");
+                String nameoffile = filePart.getSubmittedFileName();
+                db.saveDisciplinary(intid, memo, typeofmemo, inputStream, nameoffile);
+            } else {
+                db.saveDisciplinary(intid, memo, typeofmemo, null, null);
+                // response.sendRedirect("FileMemoSuccess.jsp");
+            }
+            session.removeAttribute("error");
             response.sendRedirect("FileMemoSuccess.jsp");
-           
         }
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-
-    }
-
 }
